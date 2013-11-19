1f39d69740bb343fcecd124b139473dcccb8852e
Alex Flanagan <alexander.flanagan@cbc.ca>
2013-11-19 16:17:15 -0500

    Added ant to provisioning and changed .gitignore to ignore data directory.

--- vagrant/bootstrap.cbc.dev.sh
+++ vagrant/bootstrap.cbc.dev.sh
@@ -6,6 +6,7 @@ sudo yum -y install httpd
 sudo yum -y install postgresql84-server.x86_64
 sudo yum -y install php.x86_64
 sudo yum -y install subversion
+sudo yum -y install ant
 
 # Install git (cf https://gist.github.com/eddarmitage/2001099)
 wget http://apt.sw.be/redhat/el5/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
