b9502e43e26f81d27b4ec40a3f032839a3e94ffe
Alex Flanagan <alexander.flanagan@cbc.ca>
2013-11-19 10:37:21 -0500

    Added git. wut wuuuut

--- vagrant/bootstrap.cbc.dev.sh
+++ vagrant/bootstrap.cbc.dev.sh
@@ -6,3 +6,10 @@ sudo yum -y install httpd
 sudo yum -y install postgresql84-server.x86_64
 sudo yum -y install php.x86_64
 sudo yum -y install subversion
+
+# Install git (cf https://gist.github.com/eddarmitage/2001099)
+wget http://apt.sw.be/redhat/el5/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
+rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+rpm -K rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
+rpm -i rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
+sudo yum -y install git-gui
