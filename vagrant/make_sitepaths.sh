0b12193e841e7b5d0d9fb391adfc39f5fa401c84
Alex Flanagan <alexander.flanagan@cbc.ca>
2013-11-21 13:56:54 -0500

    Mirrored directory mapping of site.

--- vagrant/make_sitepaths.sh
+++ vagrant/make_sitepaths.sh
@@ -2,4 +2,5 @@
 mkdir -p /cbc/content/sites/cbc.ca/docs/i/tests
 chown webmaster /cbc/content/sites/cbc.ca/docs/i/tests
 sudo chcon -vR --type=httpd_sys_content_t /cbc
-ln -s /cbc /var/www/html/cbc
+rm -rf /var/www/html
+ln -s /cbc/content/sites/cbc.ca/docs /var/www/html
