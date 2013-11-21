7ab9da1e751b6994ec5f64ba215106a1d6a3e7ed
Alex Flanagan <alexander.flanagan@cbc.ca>
2013-11-21 15:18:19 -0500

    Cleanup, added more scripts.

--- vagrant/make_sitepaths.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /cbc/content/sites/cbc.ca/docs/i/tests
-chown webmaster /cbc/content/sites/cbc.ca/docs/i/tests
-sudo chcon -vR --type=httpd_sys_content_t /cbc
-rm -rf /var/www/html
-ln -s /cbc/content/sites/cbc.ca/docs /var/www/html
---
+++ vagrant/scripts/make_sitepaths.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+mkdir -p /cbc/content/sites/cbc.ca/docs/i/tests
+mkdir -p /cbc/content/sites/cbc.ca/docs/i/o/globalnav/v10/css
+mkdir -p /cbc/content/sites/cbc.ca/docs/i/o/globalnav/v10/gfx
+mkdir -p /cbc/content/sites/cbc.ca/docs/i/o/globalnav/v10/ext
+mkdir -p /cbc/content/sites/cbc.ca/docs/i/o/globalnav/v10/xsl
+chown -R webmaster /cbc
+sudo chcon -vR --type=httpd_sys_content_t /cbc
+rm -rf /var/www/html
+ln -s /cbc/content/sites/cbc.ca/docs /var/www/html
