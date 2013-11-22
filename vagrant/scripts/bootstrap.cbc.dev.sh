9df845d9d3095d10de126c2c75949eef830c2c16
Adrian Maurer <adrian.maurer@gmail.com>
2013-11-22 13:41:48 -0500

    example custom script and gitignore custom files

--- vagrant/scripts/bootstrap.cbc.dev.sh
+++ vagrant/scripts/bootstrap.cbc.dev.sh
@@ -7,7 +7,7 @@ sudo yum -y install postgresql84-server.x86_64
 sudo yum -y install php.x86_64
 sudo yum -y install subversion
 sudo yum -y install ant
-sudo yum -y install tmux
+sudo yum -y install vim-X11 vim-common vim-enhanced vim-minimal
 
 # Install git (cf https://gist.github.com/eddarmitage/2001099)
 wget http://apt.sw.be/redhat/el5/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
@@ -18,3 +18,10 @@ sudo yum -y install git-gui
 
 # Overwrite default httpd.conf with our custom one.
 sudo cp /vagrant/conf/httpd.conf /etc/httpd/conf
+
+# run custom install script
+if [ -f /vagrant/scripts/custom.sh ]; then
+  sh /vagrant/scripts/custom.sh
+else
+  echo "No custom script found"
+fi
