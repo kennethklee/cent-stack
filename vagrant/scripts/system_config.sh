6078e8bd2d0eb9dc7cb02690169e557d3109d483
Alex Flanagan <alexander.flanagan@cbc.ca>
2013-11-22 14:00:21 -0500

    Added sudo to system build script.

--- vagrant/scripts/system_config.sh
+++ vagrant/scripts/system_config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Run the scripts in the proper, hackish order to get things working.
-./add_users_and_groups.sh
-./build_path_structure.sh
-./install_subversion_modules.sh
-./grant_permissions.sh
-./make_symlinks.sh
+sudo sh ./add_users_and_groups.sh
+sudo sh ./build_path_structure.sh
+sudo sh ./install_subversion_modules.sh
+sudo sh ./grant_permissions.sh
+sudo sh ./make_symlinks.sh
