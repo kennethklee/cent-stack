74f9aeeab8c0588bdfd42540e6916716c79cd4df
Alex Flanagan <alexander.flanagan@cbc.ca>
2013-11-25 09:53:46 -0500

    Moved varnish post rpmforge

--- vagrant/scripts/bootstrap.cbc.dev.sh
+++ vagrant/scripts/bootstrap.cbc.dev.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# Install everything, as root, no prompting
+# Install tools.
 sudo yum -y update
+sudo yum -y upgrade
+sudo yum -y install yum-priorities
+
+# Install webserver toolchain
 sudo yum -y install httpd
 sudo yum -y install postgresql84-server.x86_64
 sudo yum -y install php.x86_64
@@ -9,16 +13,16 @@ sudo yum -y install subversion
 sudo yum -y install ant
 sudo yum -y install vim-X11 vim-common vim-enhanced vim-minimal
 
-# Install varnish
-rpm --nosignature -i http://repo.varnish-cache.org/redhat/varnish-3.0/el5/noarch/varnish-release/varnish-release-3.0-1.el5.centos.noarch.rpm
-sudo yum -y install varnish
-
 # Install git (cf https://gist.github.com/eddarmitage/2001099)
 wget --quiet http://apt.sw.be/redhat/el5/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
 rpm -K rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
 rpm -i rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
 sudo yum -y install git-gui
+
+# Install varnish
+rpm --nosignature -i http://repo.varnish-cache.org/redhat/varnish-3.0/el5/noarch/varnish-release/varnish-release-3.0-1.el5.centos.noarch.rpm
+sudo yum -y install varnish
 
 # Overwrite default httpd.conf with our custom one.
 sudo cp /vagrant/conf/httpd.conf /etc/httpd/conf
