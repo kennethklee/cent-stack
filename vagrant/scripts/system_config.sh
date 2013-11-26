e266c2b9f779bb567a08df40bda0ae6c0491381b
Alex Flanagan <alexander.flanagan@cbc.ca>
2013-11-26 09:19:46 -0500

    Routing permissions output to /dev/null for cleanliness

--- vagrant/scripts/system_config.sh
+++ vagrant/scripts/system_config.sh
@@ -3,7 +3,7 @@
 # Run the scripts in the proper, hackish order to get things working.
 sudo sh /vagrant/scripts/add_users_and_groups.sh
 sudo sh /vagrant/scripts/build_path_structure.sh
-sudo sh /vagrant/scripts/grant_permissions.sh
+sudo sh /vagrant/scripts/grant_permissions.sh > /dev/null
 sudo sh /vagrant/scripts/install_subversion_modules.sh
-sudo sh /vagrant/scripts/grant_permissions.sh
+sudo sh /vagrant/scripts/grant_permissions.sh > /dev/null
 sudo sh /vagrant/scripts/make_symlinks.sh
